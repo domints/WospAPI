569d7b47dcf95b0e56b60073a3f35f92b98b4f7d
Szymen <domints@gmail.com>
2016-11-08 21:53:13 +0100

    fix cert

--- src/WospAPI/Program.cs
+++ src/WospAPI/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace WospAPI
 {
@@ -16,10 +17,14 @@ namespace WospAPI
 #if DEBUG
                 .UseKestrel()
 #else
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.production.json");
+                var config = builder.Build();
                 .UseKestrel(options =>
                 {
                     options.NoDelay = true;
-                    options.UseHttps("wosp.crt");
+                    options.UseHttps(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).FullName, "wosp.pfx"), config["Data:CertPass"]);
                 })
                 .UseUrls("http://*:80", "http://*:443")
 #endif
